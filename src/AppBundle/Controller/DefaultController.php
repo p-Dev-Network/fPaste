93e3ffec420bcbdc745f59734249f7783d95f857
Pedro José Anula <pedrojanula@gmail.com>
2018-04-25 17:59:46 +0200

    Updated Controller: Created Form and send to template.

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,8 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Paste;
+use AppBundle\Form\PasteType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -10,12 +12,29 @@ class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
+        $paste = new Paste();
+        $form = $this->createForm(PasteType::class, $paste);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $paste->setDate(new \DateTime("now"));
+            $paste->setDeleteDate(null);
+            $paste->setIP($this->container->get('request_stack')->getCurrentRequest()->getClientIp());
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($paste);
+            $em->flush();
+
+            return $this->redirectToRoute('homepage');
+        }
+
         return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+           'form' => $form->createView()
         ]);
     }
 }
