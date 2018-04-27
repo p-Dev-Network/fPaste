7a111f8bbf329e93159ddea0f8c223445d24d485
Pedro José Anula <pedrojanula@gmail.com>
2018-04-27 18:56:44 +0200

    Added user list to admin panel.

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -10,6 +10,34 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
  */
 class AdminController extends Controller
 {
+    /**
+     * @Route("/users", name="adminUsers")
+     */
+    public function adminUsersAction()
+    {
+        $user = $this->getUser();
 
+        if($user){
+            if($user->isAdmin()){
+                $users = $this->getDoctrine()->getRepository('AppBundle:User')->findAll();
+
+                $suspendedUsers = $this->getDoctrine()->getRepository('AppBundle:User')->findBy([
+                    'isSuspended' => true
+                ],[
+                    'signUpDate' => 'DESC'
+                ]);
+
+                return $this->render('default/Admin/users.html.twig', [
+                    'user' => $user,
+                    'users' => $users,
+                    'suspendedUsers' => $suspendedUsers
+                ]);
+            }else{
+                return $this->redirectToRoute('homepage');
+            }
+        }else{
+            return $this->redirectToRoute('login');
+        }
+    }
 
 }
